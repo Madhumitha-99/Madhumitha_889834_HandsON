95a1e564c54eeb2c63d71492fc08218bc22f73cb
MadhumithaPadmanaban <46246361+Madhumitha-99@users.noreply.github.com>
2021-03-11 21:43:05 +0530

    Handson

---
+++ ASP.NET Core/AccountDetails_EF/AccountDetails_EF/Data/AccountContext.cs
@@ -0,0 +1,18 @@
+﻿using AccountDetails_EF.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountDetails_EF.Data
+{
+    public class AccountContext:DbContext
+    {
+        public AccountContext(DbContextOptions<AccountContext> options):base(options)
+        {
+
+        }
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
---
+++ CodeAnalysis/CodeAnalysis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+        }
+    }
+}
